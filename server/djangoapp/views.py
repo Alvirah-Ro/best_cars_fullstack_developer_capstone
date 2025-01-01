30e87d1a8b31d290f0d0cc93439c22a0b90a5958
Alvirah-R0 <Bajoraneh@hotmail.com>
2025-01-01 12:40:21 -0500

    still formatting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,20 +7,17 @@ from .models import CarMake, CarModel
 from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
 from django.views.decorators.csrf import ensure_csrf_cookie
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
+
 
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -35,13 +32,15 @@ def login_user(request):
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName" : username, "status" : "Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
+
 
 # Create a `logout_request` view to handle sign out request
 def logout(request):
-    data = {"userName" : ""}
+    data = {"userName": ""}
     return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
@@ -70,11 +69,12 @@ def registration(request):
         user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName":username, "status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName":username, "error":"Already Registered"}
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
@@ -85,7 +85,8 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
+    return JsonResponse({"CarModels": cars})
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
@@ -98,7 +99,6 @@ def get_dealerships(request, state="All"):
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-
 def get_dealer_reviews(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
@@ -122,23 +122,22 @@ def get_dealer_reviews(request, dealer_id):
             else:
                 review_detail["sentiment"] = "unknown"
 
-        return JsonResponse({"status" : 200, "reviews" : reviews})
+        return JsonResponse({"status": 200, "reviews": reviews})
     else:
-        return JsonResponse([{"status" : 400, "message" : "Bad Request"}])
+        return JsonResponse([{"status": 400, "message": "Bad Request"}])
 
 
 # Create a `get_dealer_details` view to render the dealer details
-
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status" : 200,"dealer" : dealership})
+        return JsonResponse({"status": 200,"dealer": dealership})
     else:
-        return JsonResponse({"status" : 400,"message" : "Bad Request"})
+        return JsonResponse({"status": 400,"message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
-
 @require_http_methods(["POST"])
 def add_review(request):
     if(request.user.is_anonymous == False):
@@ -148,15 +147,20 @@ def add_review(request):
         try:
             response = post_review(data)
             print("API response:", response)
-            return JsonResponse({"status":200})
+            return JsonResponse({"status": 200})
         except json.JSONDecodeError:
             print("JSONDecodeError: Invalid JSON in request body.")
-            return JsonResponse({"status" : 400, "message" : "Invalid JSON format"})
-        except:
-            return JsonResponse({"status" : 401,"message" : "Error in posting review"})
+            return JsonResponse(
+                {"status": 400, "message": "Invalid JSON format"})
+        except Exception as e:
+            print(f"Error: {e}")
+            return JsonResponse(
+                {"status": 401,"message": "Error in posting review"})
     else:
-        return JsonResponse({"status" : 403,"message" : "Unauthorized"})
+        return JsonResponse({"status": 403,"message": "Unauthorized"})
+
 
 @ensure_csrf_cookie
 def index_view(request):
-    return render(request, 'react_index.html')  # passes through Django templates
+    return render(request, 'react_index.html')  
+    # passes through Django templates
